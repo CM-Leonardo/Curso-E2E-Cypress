459f01ef0289132fb95eda53a8ba87a553f6fe45
Leonard-Coneglian <leonardo.coneglian@cotefacil.com>
2025-10-27 18:21:23 -0300

    npm lint:fix

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -4,16 +4,16 @@ import { fillSignupFormAndSubmit } from '../functions/commands'
 describe('Sign Up', () => {
 
   it.only('Successfully signs up using confirmation code sent via email (Usando função)', () => {
-        fillSignupFormAndSubmit(user)
+    fillSignupFormAndSubmit(user)
 
-        cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
-        cy.contains('a', 'Create a new note').should('be.visible')
+    cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
   })
- 
-  it('Successfully signs up using confirmation code sent via email (Usando Commands)', () => {
-        cy.fillSignupFormAndSubmit(user)
 
-        cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
-        cy.contains('a', 'Create a new note').should('be.visible')
+  it('Successfully signs up using confirmation code sent via email (Usando Commands)', () => {
+    cy.fillSignupFormAndSubmit(user)
+
+    cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
--- cypress/functions/commands.js
+++ cypress/functions/commands.js
@@ -1,16 +1,16 @@
 export function fillSignupFormAndSubmit(data){
-    cy.visit('/signup')
+  cy.visit('/signup')
 
-    cy.get('#email').type(data.email)
-    cy.get('#password').type(data.password, { log: false })
-    cy.get('#confirmPassword').type(data.password, { log: false })
-     cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
+  cy.get('#email').type(data.email)
+  cy.get('#password').type(data.password, { log: false })
+  cy.get('#confirmPassword').type(data.password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
 
-    cy.mailosaurGetMessage(data.serverID, {
-        sentTo: data.email
-    }).then(message => {
-        const confirmationCode = message.html.body.match(/\d{6}/)[0]
-        cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-    })
+  cy.mailosaurGetMessage(data.serverID, {
+    sentTo: data.email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+  })
 }
