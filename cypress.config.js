e6402a3411c093da2229098b7963bc675f5cae69
Leonard-Coneglian <leonardo.coneglian@cotefacil.com>
2025-10-30 12:15:11 -0300

    teste EsLint

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,6 @@ module.exports = defineConfig({
   projectId: "jkf32g",
   chromeWebSecurity: false,
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    // },
     baseUrl: 'https://notes-serverless-app.com/',
     env: {
       viewportWidthBreakpoint: 768,
